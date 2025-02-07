7be36d6988bc28cee3421f1638d04299930aab36
Jose Cardona <jpcardonamena@gmail.com>
2025-02-07 15:52:10 +0100

    added cors

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,19 @@ import express from "express";
 import bodyParser from "body-parser";
 import calendarRoutes from "./routes/calendarRoutes";
 import {setupSwagger} from "./utils/swagger";
+import cors from "cors";
 
 dotenv.config();
-
-const app = express();
 const PORT = process.env.PORT || 4002;
 
+const app = express();
+
+const corsOptions = {
+    origin: process.env.FRONTEND_URL, // Your frontend's URL
+    credentials: true, // Allow credentials (cookies, headers)
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 setupSwagger(app);
 app.use("/calendar", calendarRoutes);
